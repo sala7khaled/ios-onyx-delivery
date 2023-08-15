bac0f1e4e338e0784a59200b5f4ac01ef28e9cd2
Salah Khaled <sala7khaled7@gmail.com>
2023-08-15 14:31:22 +0300

    added api headers

---
+++ ios-onyx-delivery/Core/Network/Components/APIHeader.swift
@@ -0,0 +1,20 @@
+//
+//  APIHeader.swift
+//  ios-onyx-delivery
+//
+//  Created by Salah's iMac on 15/08/2023.
+//
+
+import Foundation
+
+enum APIHeader {
+    
+    static let accept = "Accept"
+    static let contentType = "Content-Type"
+    static let locale = "Accept-Language"
+    static let platform = "platform"
+    static let deviceId = "deviceId"
+    static let version = "version"
+    static let build = "build"
+    static let authorization = "Authorization"
+}
